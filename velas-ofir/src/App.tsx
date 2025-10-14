4f42c0c107fabf3253dc63435e36a88ed0105e36
Gustavo Santos de Campos <gustavo.s.d.c@outlook.com>
2025-10-14 00:03:43 -0300

    build test

--- velas-ofir/src/App.tsx
+++ velas-ofir/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className='flex flex-col items-center w-screen h-screen justify-center gap-4'>
         <img src={logoPreto} alt="logo-ofir." className='w-40' />
-        <h1 className='text-3xl font-quintessential font-light'><span className='font-medium'>15.10</span> - Lançamento oficial</h1>
+        <h1 className='text-3xl font-quintessential font-light'><span className='font-bold'>15.10</span> - Lançamento oficial</h1>
         <a href="https://www.instagram.com/velasofir/" target="_blank" rel="noopener noreferrer" className='text-xl font-quintessential font-light underline italic'>Instagram</a>
       </div>
     </>
