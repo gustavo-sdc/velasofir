ff0ef48249931dd0d4a7d48a4463b715ac94949a
Gustavo Santos de Campos <gustavo.s.d.c@outlook.com>
2025-10-13 23:57:43 -0300

    build

--- velas-ofir/vite.config.ts
+++ velas-ofir/vite.config.ts
@@ -5,14 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  theme: {
-    extend: {
-      fontFamily: {
-        playfair: ['Playfair-Display', 'sans-serif'],
-        quintessential: ['Quintessential', 'cursive'],
-      },
-    },
-  },
   plugins: [
     react(),
     tailwindcss(),
